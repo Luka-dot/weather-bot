cf3dfd7d0b324c8c52880cc9185e18a71a3eb0a0
Luka-dot <79lukask@gmail.com>
2021-03-21 19:22:55 -0400

    api weather bot for telegram

---
+++ functions/index.js
@@ -0,0 +1,23 @@
+const functions = require("firebase-functions");
+const Telegraf = require("telegraf");
+
+let config = require("../env.json");
+
+if (Object.keys(functions.config()).length) {
+    config = functions.config();
+}
+
+const bot = new Telegraf(config.service.telegram_key);
+bot.start((ctx) => ctx.reply("Welcome"));
+bot.help((ctx) => ctx.reply("Send me a sticker"));
+bot.on("sticker", (ctx) => ctx.reply("👍"));
+bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.launch();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase! and Me!!!");
+});
